ae3b0707a6b7f793c90982a9108bee03196f8ade
stepler662 <stepuro101@mail.ru>
2025-08-06 00:13:58 +0300

    Hw1

---
+++ src/main/java/c1/lesson1/HW1.java
@@ -0,0 +1,13 @@
+package c1.lesson1;
+
+public class HW1 {
+    public static void main(String[] args) {
+        printThreeWords();
+
+    }
+    public static void printThreeWords(){
+        System.out.print("Orange");
+        System.out.print("Banana");
+        System.out.print("Apple");
+    }
+}
---
+++ src/main/java/c1/lesson2/MethodApp.java
@@ -0,0 +1,7 @@
+package c1.lesson2;
+
+public class MethodApp {
+    public static void main(String[] args) {
+
+    }
+}
